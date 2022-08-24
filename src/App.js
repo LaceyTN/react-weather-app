869ef8c799dc129b4326d08cc44c9eb45851e1e8
LaceyTN <103006938+LaceyTN@users.noreply.github.com>
2022-08-24 18:28:45 -0500

    added to link

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project is coded by{" "}
-        <a href="" target="_blank">
+        <a href="" target="_blank" rel="noreferrer">
           Lacey Chaney
         </a>{" "}
         and is{" "}
-        <a href="https://github.com/LaceyTN/react-weather-app" target="_blank">
+        <a
+          href="https://github.com/LaceyTN/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           open-sourced on GitHub
         </a>
       </footer>
