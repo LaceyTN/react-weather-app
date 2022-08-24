7f4fa10458dd03b95a35615b1dcd0fbc3da4a61c
LaceyTN <103006938+LaceyTN@users.noreply.github.com>
2022-08-24 17:24:54 -0500

    added GitHub url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by{" "}
+        <a href="https://peppy-pika-fa046e.netlify.app" target="_blank">
+          Lacey Chaney
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/LaceyTN/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
