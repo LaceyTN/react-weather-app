a5bdd4ea6f5d5dea843be0eb1f34860f4c3f836b
LaceyTN <103006938+LaceyTN@users.noreply.github.com>
2022-08-25 17:53:36 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by{" "}
-        <a
-          href="https://peppy-pika-fa046e.netlify.app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Lacey Chaney
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/LaceyTN/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://peppy-pika-fa046e.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Lacey Chaney
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/LaceyTN/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
